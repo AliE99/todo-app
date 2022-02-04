9265a612b201ed44b3531956c031ee96e69cc09e
Ali Elahi <alielahi.1378@gmail.com>
2022-02-04 18:18:36 +0330

    The upper part of the UI has been created

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,19 +1,78 @@
 // ignore_for_file: prefer_const_constructors_in_immutables, use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:todo_app/screens/constants.dart';
 
 class TasksScreen extends StatelessWidget {
   TasksScreen();
 
   @override
   Widget build(BuildContext context) {
+    MediaQueryData query = MediaQuery.of(context);
+    final height = query.size.height;
+    final width = query.size.width;
     return Scaffold(
-      backgroundColor: Colors.lightBlueAccent,
+      backgroundColor: primaryColor,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: primaryColor,
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
       body: SafeArea(
         child: Column(
-          children: <Widget>[
-            Icon(Icons.list),
-            Text('TODO'),
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.only(
+                top: 0.1 * width,
+                left: 0.05 * width,
+                right: 0.05 * width,
+                bottom: 0.05 * width,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  CircleAvatar(
+                    child: Icon(
+                      Icons.list,
+                      color: primaryColor,
+                      size: 0.1 * width,
+                    ),
+                    backgroundColor: secondaryColor,
+                    radius: 0.07 * width,
+                  ),
+                  SizedBox(
+                    height: 0.01 * height,
+                  ),
+                  Text(
+                    'TODO App',
+                    style: TextStyle(
+                      color: secondaryColor,
+                      fontSize: 0.09 * width,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  Text(
+                    '12 Tasks',
+                    style: TextStyle(
+                      color: secondaryColor,
+                      fontSize: 0.04 * width,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: secondaryColor,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(0.1 * width),
+                    topRight: Radius.circular(0.1 * width),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
