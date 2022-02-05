d44ffb517056aa3a0085cf98c2bb7363c4bff90d
Ali Elahi <alielahi.1378@gmail.com>
2022-02-05 17:59:15 +0330

    State has been managed in task tile

--- lib/screens/widgets/task_tile.dart
+++ lib/screens/widgets/task_tile.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/screens/constants.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a Task'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (value) {},
+      title: Text(
+        'This is a Task',
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
       ),
+      trailing: TaskCheckbox(
+        checkboxState: isChecked,
+        toggleCheckboxState: (value) {
+          setState(() {
+            isChecked = value!;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool checkboxState;
+  final Function(bool?) toggleCheckboxState;
+
+  TaskCheckbox(
+      {required this.checkboxState, required this.toggleCheckboxState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: primaryColor,
+      value: checkboxState,
+      onChanged: toggleCheckboxState,
     );
   }
 }
